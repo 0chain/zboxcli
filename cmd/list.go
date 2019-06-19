e551a080314d6ccd7ac834b064ac625dd55717a4
Sarath Ambati <sarath@0chain.net>
2019-06-19 13:44:19 +0530

    adding the list cmd with auth ticket to get the lookup hash

--- cmd/list.go
+++ cmd/list.go
@@ -5,9 +5,9 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/0chain/zboxcmd/util"
 	"github.com/0chain/gosdk/zboxcore/fileref"
 	"github.com/0chain/gosdk/zboxcore/sdk"
+	"github.com/0chain/zboxcmd/util"
 	"github.com/spf13/cobra"
 )
 
@@ -18,37 +18,70 @@ var listCmd = &cobra.Command{
 	Long:  `list files from blobbers`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		fflags := cmd.Flags()                      // fflags is a *flag.FlagSet
-		if fflags.Changed("allocation") == false { // check if the flag "path" is set
-			fmt.Println("Error: allocation flag is missing") // If not, we'll let the user know
-			return                                           // and return
-		}
-		if fflags.Changed("remotepath") == false {
-			fmt.Println("Error: remotepath flag is missing")
+		fflags := cmd.Flags() // fflags is a *flag.FlagSet
+		if fflags.Changed("remotepath") == false && fflags.Changed("authticket") == false {
+			fmt.Println("Error: remotepath / authticket flag is missing")
 			return
 		}
-		allocationID := cmd.Flag("allocation").Value.String()
-		allocationObj, err := sdk.GetAllocation(allocationID)
-		if err != nil {
-			fmt.Println("Error fetching the allocation", err)
-			return
-		}
+
 		remotepath := cmd.Flag("remotepath").Value.String()
-		ref, err := allocationObj.ListDir(remotepath)
-		if err != nil {
-			fmt.Println(err.Error())
+		authticket := cmd.Flag("authticket").Value.String()
+		lookuphash := cmd.Flag("lookuphash").Value.String()
+		if len(remotepath) == 0 && (len(authticket) == 0 || len(lookuphash) == 0) {
+			fmt.Println("Error: remotepath / authticket / lookuphash flag is missing")
 			return
 		}
-		header := []string{"Type", "Name", "Path", "Size", "Num Blocks"}
-		data := make([][]string, len(ref.Children))
-		for idx, child := range ref.Children {
-			size := strconv.FormatInt(child.Size, 10)
-			if child.Type == fileref.DIRECTORY {
-				size = ""
+
+		if len(remotepath) > 0 {
+			if fflags.Changed("allocation") == false { // check if the flag "path" is set
+				fmt.Println("Error: allocation flag is missing") // If not, we'll let the user know
+				return                                           // and return
 			}
-			data[idx] = []string{child.Type, child.Name, child.Path, size, strconv.FormatInt(child.NumBlocks, 10)}
+			allocationID := cmd.Flag("allocation").Value.String()
+			allocationObj, err := sdk.GetAllocation(allocationID)
+			if err != nil {
+				fmt.Println("Error fetching the allocation", err)
+				return
+			}
+			remotepath := cmd.Flag("remotepath").Value.String()
+			ref, err := allocationObj.ListDir(remotepath)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			header := []string{"Type", "Name", "Path", "Size", "Num Blocks", "Lookup Hash"}
+			data := make([][]string, len(ref.Children))
+			for idx, child := range ref.Children {
+				size := strconv.FormatInt(child.Size, 10)
+				if child.Type == fileref.DIRECTORY {
+					size = ""
+				}
+				data[idx] = []string{child.Type, child.Name, child.Path, size, strconv.FormatInt(child.NumBlocks, 10), child.LookupHash}
+			}
+			util.WriteTable(os.Stdout, header, []string{}, data)
+		} else if len(authticket) > 0 {
+			allocationObj, err := sdk.GetAllocationFromAuthTicket(authticket)
+			if err != nil {
+				fmt.Println("Error fetching the allocation", err)
+				return
+			}
+			ref, err := allocationObj.ListDirFromAuthTicket(authticket, lookuphash)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			header := []string{"Type", "Name", "Size", "Num Blocks", "Lookup Hash"}
+			data := make([][]string, len(ref.Children))
+			for idx, child := range ref.Children {
+				size := strconv.FormatInt(child.Size, 10)
+				if child.Type == fileref.DIRECTORY {
+					size = ""
+				}
+				data[idx] = []string{child.Type, child.Name, size, strconv.FormatInt(child.NumBlocks, 10), child.LookupHash}
+			}
+			util.WriteTable(os.Stdout, header, []string{}, data)
 		}
-		util.WriteTable(os.Stdout, header, []string{}, data)
+
 		return
 	},
 }
@@ -57,6 +90,7 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 	listCmd.PersistentFlags().String("allocation", "", "Allocation ID")
 	listCmd.PersistentFlags().String("remotepath", "", "Remote path to list from")
+	downloadCmd.PersistentFlags().String("authticket", "", "Auth ticket fot the file to download if you dont own it")
+	downloadCmd.PersistentFlags().String("lookuphash", "", "The remote lookuphash of the object retrieved from the list")
 	listCmd.MarkFlagRequired("allocation")
-	listCmd.MarkFlagRequired("remotepath")
 }
