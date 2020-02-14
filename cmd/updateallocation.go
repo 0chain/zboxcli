46397da1d33322a7c3e9e678f8c411512c8e6c1a
Krishna <kupadhyaya@techracers.io>
2020-02-14 14:52:01 +0530

    Update updateallocation to include allocationID

--- cmd/updateallocation.go
+++ cmd/updateallocation.go
@@ -17,11 +17,17 @@ var updateallocationCmd = &cobra.Command{
 	Long:  `Updates allocation's expiry and size`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
+		fflags := cmd.Flags()                      // fflags is a *flag.FlagSet
+		if fflags.Changed("allocation") == false { // check if the flag "path" is set
+			PrintError("Error: allocation flag is missing") // If not, we'll let the user know
+			os.Exit(1)                                      // and return
+		}
 		if updateSize == nil || updateExpiry == nil {
 			PrintError("Invalid updateallocation parameters.")
 			os.Exit(1)
 		}
-		txnHash, err := sdk.UpdateAllocation(*updateSize, *updateExpiry)
+		allocationID := cmd.Flag("allocation").Value.String()
+		txnHash, err := sdk.UpdateAllocation(*updateSize, *updateExpiry, allocationID)
 		if err != nil {
 			PrintError("Error creating allocation." + err.Error())
 			os.Exit(1)
@@ -33,8 +39,10 @@ var updateallocationCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(updateallocationCmd)
+	updateallocationCmd.PersistentFlags().String("allocation", "", "Allocation ID")
 	updateSize = updateallocationCmd.PersistentFlags().Int64("size", 2147483648, "--size 10000")
 	updateExpiry = updateallocationCmd.PersistentFlags().Int64("expiry", 2592000, "--size 10000")
+	updateallocationCmd.MarkFlagRequired("allocation")
 	updateallocationCmd.MarkFlagRequired("size")
 	updateallocationCmd.MarkFlagRequired("expiry")
 }
