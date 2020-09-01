eb12351b5bd7441e54d7921f51ec1388715eafd1
Krishna <kupadhyaya@techracers.io>
2020-09-01 15:06:00 +0530

    Add sign-data method

--- cmd/walletinfo.go
+++ cmd/walletinfo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/0chain/gosdk/core/encryption"
 	"github.com/0chain/gosdk/zboxcore/client"
 	"github.com/0chain/zboxcli/util"
 
@@ -41,7 +42,32 @@ var walletinfoCmd = &cobra.Command{
 	},
 }
 
+var signCmd = &cobra.Command{
+	Use:   "sign-data",
+	Short: "Sign given data",
+	Long:  `Sign given data`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		data, _ := cmd.Flags().GetString("data")
+		if data == "" {
+			data = client.GetClientID()
+		} else {
+			data = encryption.Hash(data)
+		}
+		sign, err := client.Sign(data)
+		if err != nil {
+			fmt.Println("Error generating the signature. ", err.Error())
+			return
+		}
+		fmt.Println("Signature : " + sign)
+		return
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(walletinfoCmd)
 	walletinfoCmd.Flags().Bool("json", false, "pass this option to print response as json data")
+
+	rootCmd.AddCommand(signCmd)
+	signCmd.Flags().String("data", "", "give data for signing, Default will be clientID")
 }
