9dd435aa32c9dade746f95d3430163b6f831f6de
Lz <code@geax.io>
2021-06-19 13:58:52 +0800

    fix(upload): 0chain/zboxcli#36 removed test scripts

--- sp_lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./zbox sp-lock --blobber_id f65af5d64000c7cd2883f4910eb69086f9d6e6635c744e62afcfab58b938ee25 --tokens 1 --verbose
-./zbox sp-lock --blobber_id 7a90e6790bcd3d78422d7a230390edc102870fe58c15472073922024985b1c7d --tokens 1 --verbose
-./zbox sp-lock --blobber_id 2f051ca6447d8712a020213672bece683dbd0d23a81fdf93ff273043a0764d18 --tokens 1 --verbose
