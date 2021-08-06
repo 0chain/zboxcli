5585dc47dff4128418b5b381750791681de63451
Piers Shepperson <piers.shepperson@btinternet.com>
2021-08-06 10:45:34 +0100

    get-mpt directly view blockchain mpt (#74)

---
+++ cmd/getmpt.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/0chain/gosdk/zboxcore/sdk"
+	"github.com/spf13/cobra"
+)
+
+var getMptKeyCommand = &cobra.Command{
+	Use:   "get-mpt",
+	Short: "Directly view blockchain data",
+	Long:  `Directly view blockchain data from MPT key`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		if cmd.Flags().Changed("key") == false {
+			log.Fatal("Required Mpt key missing\n")
+		}
+		key := cmd.Flag("key").Value.String()
+		jsonBytes, err := sdk.GetMptData(key)
+		if err != nil {
+			log.Fatalf("Failed to get Mpt key: %v\n", err)
+		}
+
+		var indented bytes.Buffer
+		err = json.Indent(&indented, jsonBytes, "", "\t")
+		if err != nil {
+			log.Fatalf("Result %s baddly formated: %v\n", string(jsonBytes), err)
+		}
+
+		noBackSlash := strings.Replace(indented.String(), "\\", "", -1)
+		fmt.Println(key, ": ", noBackSlash)
+		return
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getMptKeyCommand)
+	getMptKeyCommand.PersistentFlags().String("key", "", "Key into MPT datastore")
+	getMptKeyCommand.MarkFlagRequired("key")
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/0chain/zboxcli
 go 1.13
 
 require (
-	github.com/0chain/gosdk v1.2.80-0.20210730183026-29ea38eb11a3
+	github.com/0chain/gosdk v1.2.82-0.20210806092509-faf62ccf8f8f
+	github.com/lithammer/shortuuid v3.0.0+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-runewidth v0.0.10 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
