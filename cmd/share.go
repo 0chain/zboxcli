851d3712e5a0cbc461e8d1410d9f8f55abc6f4d9
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-06-04 10:56:14 +0545

    Add who-pays in authticket

--- cmd/share.go
+++ cmd/share.go
@@ -19,21 +19,23 @@ var shareCmd = &cobra.Command{
 	Long:  `share files from blobbers`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		fflags := cmd.Flags()                      // fflags is a *flag.FlagSet
-		if fflags.Changed("allocation") == false { // check if the flag "path" is set
+		fflags := cmd.Flags()              // fflags is a *flag.FlagSet
+		if !fflags.Changed("allocation") { // check if the flag "path" is set
 			PrintError("Error: allocation flag is missing") // If not, we'll let the user know
 			os.Exit(1)                                      // and os.Exit(1)
 		}
-		if fflags.Changed("remotepath") == false {
+		if !fflags.Changed("remotepath") {
 			PrintError("Error: remotepath flag is missing")
 			os.Exit(1)
 		}
+
 		allocationID := cmd.Flag("allocation").Value.String()
 		allocationObj, err := sdk.GetAllocation(allocationID)
 		if err != nil {
 			PrintError("Error fetching the allocation", err)
 			os.Exit(1)
 		}
+
 		remotepath := cmd.Flag("remotepath").Value.String()
 		refType := fileref.FILE
 		statsMap, err := allocationObj.GetFileStats(remotepath)
@@ -41,7 +43,8 @@ var shareCmd = &cobra.Command{
 			PrintError("Error in getting information about the object." + err.Error())
 			os.Exit(1)
 		}
-		isFile := false
+
+		var isFile bool
 		for _, v := range statsMap {
 			if v != nil {
 				isFile = true
@@ -86,9 +89,16 @@ var shareCmd = &cobra.Command{
 				}
 				availableAfter = *aa
 			}
-
+			whoPays, err := cmd.Flags().GetInt("who-pays")
+			if err != nil {
+				PrintError(err.Error())
+				os.Exit(1)
+			}
 			encryptionpublickey := cmd.Flag("encryptionpublickey").Value.String()
-			ref, err := allocationObj.GetAuthTicket(remotepath, fileName, refType, refereeClientID, encryptionpublickey, expiration, &availableAfter)
+			ref, err := allocationObj.GetAuthTicket(
+				remotepath, fileName, refType, refereeClientID, encryptionpublickey,
+				whoPays, expiration, &availableAfter)
+
 			if err != nil {
 				PrintError(err.Error())
 				os.Exit(1)
@@ -107,6 +117,7 @@ func init() {
 	shareCmd.PersistentFlags().Int64("expiration-seconds", 0, "Authticket will expire when the seconds specified have elapsed after the instant of its creation")
 	shareCmd.PersistentFlags().String("available-after", "", "Timelock for private file that makes the file available for download at certain time. 4 input formats are supported: +1h30m, +30, 1647858200 and 2022-03-21 10:21:38. Default value is current local time.")
 	shareCmd.PersistentFlags().Bool("revoke", false, "Revoke share for remotepath")
+	shareCmd.PersistentFlags().Int("who-pays", 0, "Who Pays; Owner or 3rd party. Default is owner")
 	shareCmd.MarkFlagRequired("allocation")
 	shareCmd.MarkFlagRequired("remotepath")
 }
