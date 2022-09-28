01b0860b516ee5261e8c4f814529d3bbf515216c
Lz <imlangzi@qq.com>
2022-09-28 10:13:51 +0800

    feat(commit): removed commit

--- cmd/common.go
+++ cmd/common.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"sync"
 
-	"github.com/0chain/gosdk/core/transaction"
 	"gopkg.in/cheggaaa/pb.v1"
 )
 
@@ -49,17 +48,6 @@ func (s *StatusBar) Error(allocationID string, filePath string, op int, err erro
 	}
 
 	PrintError("Error in file operation:", errDetail)
-}
-
-func (s *StatusBar) CommitMetaCompleted(request, response string, txn *transaction.Transaction, err error) {
-	defer s.wg.Done()
-	if err != nil {
-		s.success = false
-		PrintError("Error in commitMetaTransaction." + err.Error())
-	} else {
-		s.success = true
-		fmt.Println("Commit Metadata successful, Response :", response)
-	}
 }
 
 func (s *StatusBar) RepairCompleted(filesRepaired int) {
