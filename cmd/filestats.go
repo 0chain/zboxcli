963f3318e4da784366709e4f3fa5600ca8684443
Manohar Reddy <b.manu199@gmail.com>
2023-03-14 22:42:51 +0100

    init testing AuthorizerHealthCheck

--- cmd/filestats.go
+++ cmd/filestats.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
+	"context"
 	"os"
-	"strconv"
 
-	"github.com/0chain/gosdk/zboxcore/sdk"
-	"github.com/0chain/zboxcli/util"
+	"github.com/0chain/gosdk/zcnbridge/transaction"
+	"github.com/0chain/gosdk/zcncore"
 	"github.com/spf13/cobra"
 )
 
@@ -16,46 +16,61 @@ var statsCmd = &cobra.Command{
 	Long:  `stats for file from blobbers`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		fflags := cmd.Flags()                      // fflags is a *flag.FlagSet
-		if fflags.Changed("allocation") == false { // check if the flag "path" is set
-			PrintError("Error: allocation flag is missing") // If not, we'll let the user know
-			os.Exit(1)                                      // and return
-		}
-		if fflags.Changed("remotepath") == false {
-			PrintError("Error: remotepath flag is missing")
-			os.Exit(1)
-		}
-		allocationID := cmd.Flag("allocation").Value.String()
-		doJSON, _ := cmd.Flags().GetBool("json")
+		// fflags := cmd.Flags()                      // fflags is a *flag.FlagSet
+		// if fflags.Changed("allocation") == false { // check if the flag "path" is set
+		// 	PrintError("Error: allocation flag is missing") // If not, we'll let the user know
+		// 	os.Exit(1)                                      // and return
+		// }
+		// if fflags.Changed("remotepath") == false {
+		// 	PrintError("Error: remotepath flag is missing")
+		// 	os.Exit(1)
+		// }
+		// allocationID := cmd.Flag("allocation").Value.String()
+		// doJSON, _ := cmd.Flags().GetBool("json")
 
-		allocationObj, err := sdk.GetAllocation(allocationID)
-		if err != nil {
-			PrintError("Error fetching the allocation", err)
-			os.Exit(1)
+		// allocationObj, err := sdk.GetAllocation(allocationID)
+		// if err != nil {
+		// 	PrintError("Error fetching the allocation", err)
+		// 	os.Exit(1)
+		// }
+		// remotepath := cmd.Flag("remotepath").Value.String()
+		// ref, err := allocationObj.GetFileStats(remotepath)
+		// if err != nil {
+		// 	PrintError(err.Error())
+		// 	os.Exit(1)
+		// }
+		// if doJSON {
+		// 	util.PrintJSON(ref)
+		// 	return
+		// }
+
+		payload := &zcncore.AuthorizerHealthCheckPayload{
+			ID: "0",
 		}
-		remotepath := cmd.Flag("remotepath").Value.String()
-		ref, err := allocationObj.GetFileStats(remotepath)
+
+		ctx := context.TODO()
+		trx, err := transaction.AuthorizerHealthCheck(ctx, payload)
 		if err != nil {
 			PrintError(err.Error())
 			os.Exit(1)
 		}
-		if doJSON {
-			util.PrintJSON(ref)
-			return
-		}
-		header := []string{"Blobber", "Name", "Path", "Size", "Uploads", "Block Downloads", "Challenges", "Blockchain Aware"}
-		data := make([][]string, 0)
-		idx := 0
-		for k, v := range ref {
-			if v != nil {
-				size := strconv.FormatInt(v.Size, 10)
-				rowData := []string{k, v.Name, v.Path, size, strconv.FormatInt(v.NumUpdates, 10), strconv.FormatInt(v.NumBlockDownloads, 10), strconv.FormatInt(v.SuccessChallenges, 10), strconv.FormatBool(len(v.WriteMarkerRedeemTxn) > 0)}
-				data = append(data, rowData)
-				idx++
-			}
-		}
 
-		util.WriteTable(os.Stdout, header, []string{}, data)
+		err = trx.Verify(ctx)
+		PrintError(err.Error())
+
+		// header := []string{"Blobber", "Name", "Path", "Size", "Uploads", "Block Downloads", "Challenges", "Blockchain Aware"}
+		// data := make([][]string, 0)
+		// idx := 0
+		// for k, v := range ref {
+		// 	if v != nil {
+		// 		size := strconv.FormatInt(v.Size, 10)
+		// 		rowData := []string{k, v.Name, v.Path, size, strconv.FormatInt(v.NumUpdates, 10), strconv.FormatInt(v.NumBlockDownloads, 10), strconv.FormatInt(v.SuccessChallenges, 10), strconv.FormatBool(len(v.WriteMarkerRedeemTxn) > 0)}
+		// 		data = append(data, rowData)
+		// 		idx++
+		// 	}
+		// }
+
+		// util.WriteTable(os.Stdout, header, []string{}, data)
 		return
 	},
 }
