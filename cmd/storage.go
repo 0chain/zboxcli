6f83215c3646b7741d34212920a60d0041babb9f
Piers Shepperson <piers.shepperson@btinternet.com>
2023-03-28 17:11:59 +0100

    bl-info

--- cmd/storage.go
+++ cmd/storage.go
@@ -160,6 +160,7 @@ var blobberInfoCmd = &cobra.Command{
 		fmt.Println("last_health_check:", blob.LastHealthCheck.ToTime())
 		fmt.Println("capacity_used:    ", blob.Allocated)
 		fmt.Println("total_stake:      ", blob.TotalStake)
+		fmt.Println("is_available:     ", blob.IsAvailable)
 		fmt.Println("terms:")
 		fmt.Println("  read_price:        ", blob.Terms.ReadPrice, "/ GB")
 		fmt.Println("  write_price:       ", blob.Terms.WritePrice, "/ GB")
