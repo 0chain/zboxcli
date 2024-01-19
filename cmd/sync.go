a7ab229d50e83d2c268fce5152eed88e7b4d6158
Roshan-Mehta <roshankr.kumar222@gmail.com>
2024-01-19 18:10:02 +0530

    Fix build

--- cmd/sync.go
+++ cmd/sync.go
@@ -230,11 +230,11 @@ var syncCmd = &cobra.Command{
 		downloadSlice := make([]MultiDownloadOption, 0)
 		downloadStatusBars := make([]*StatusBar, 0)
 		for _, f := range lDiff {
-			os := runtime.GOOS
+			operatingSys := runtime.GOOS
 			// Check the operating system and modify the path accordingly
-			if os == "windows" {
+			if operatingSys == "windows" {
 				f.Path = strings.ReplaceAll(f.Path, "/", "\\")
-			} 
+			}
 			localpath = strings.TrimRight(localpath, "/")
 			lPath := localpath + f.Path
 			fileRemotePath, err := getFullRemotePath(f.Path, remotepath)
