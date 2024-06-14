0b4220fa6df850c3e9102d8a519eec78704e9728
storybehind <arunmail12@gmail.com>
2024-06-14 13:48:21 +0530

    repair-size command

--- cmd/repair.go
+++ cmd/repair.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"sync"
 
@@ -55,12 +57,60 @@ var startRepair = &cobra.Command{
 	},
 }
 
+var repairSize = &cobra.Command{
+	Use:   "repair-size",
+	Short: "gets only size to repair file to blobbers",
+	Long:  `gets only size to repair file to blobbers`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		fflags := cmd.Flags()                     
+		if !fflags.Changed("allocation") { 
+			PrintError("Error: allocation flag is missing")
+			os.Exit(1)                                     
+		}
+
+		repairPath := "/"
+		var err error
+		if fflags.Changed("repairpath") {
+			if repairPath, err = fflags.GetString("repairpath"); err != nil {
+				PrintError("Error: repairpath is not of string type", err)
+				os.Exit(1)
+			}
+		}
+
+		allocationID := cmd.Flag("allocation").Value.String()
+		allocationObj, err := sdk.GetAllocation(allocationID)
+		if err != nil {
+			PrintError("Error fetching the allocation.", err)
+			os.Exit(1)
+		}
+
+		size, err := allocationObj.RepairSize(repairPath)
+		if err != nil {
+			PrintError("get repair size failed: ", err)
+			os.Exit(1)
+		}
+
+		jsonBytes, err := json.Marshal(size)
+		if err != nil {
+			PrintError("error marshaling size: ", err)
+			os.Exit(1)
+		}
+		fmt.Println(string(jsonBytes))
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(startRepair)
+	rootCmd.AddCommand(repairSize)
 	startRepair.PersistentFlags().String("allocation", "", "Allocation ID")
 	startRepair.PersistentFlags().String("rootpath", "", "File path for local files ")
 	startRepair.PersistentFlags().String("repairpath", "", "Path to repair")
 	startRepair.MarkFlagRequired("allocation")
 	startRepair.MarkFlagRequired("rootpath")
 	startRepair.MarkFlagRequired("repairpath")
+
+	repairSize.PersistentFlags().String("allocation", "", "Allocation ID")
+	repairSize.PersistentFlags().String("repairpath", "", "Path to repair")
+	repairSize.MarkFlagRequired("allocation")
 }
